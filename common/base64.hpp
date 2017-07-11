4420ff3fa3255de20cad65fc013c2095bfef0f09
Yoshitake Hamano <oed0cow6oy5@gmail.com>
2017-07-11 23:34:17 +0900

    Support old gcc

--- common/base64.hpp
+++ common/base64.hpp
@@ -10,10 +10,10 @@ namespace makerfaire
     {
         using namespace std;
         using namespace boost::archive::iterators;
-        using InputItr  = istreambuf_iterator<char>;
-        using OutputItr = ostream_iterator<char>;
-        using EncodeItr = base64_from_binary<transform_width<InputItr, 6, 8>>;
-        using DecodeItr = transform_width<binary_from_base64<InputItr>, 8, 6, char>;
+        typedef istreambuf_iterator<char> InputItr;
+        typedef ostream_iterator<char> OutputItr;
+        typedef base64_from_binary<transform_width<InputItr, 6, 8>> EncodeItr;
+        typedef transform_width<binary_from_base64<InputItr>, 8, 6, char> DecodeItr;
         
         template <typename InputStream, typename OutputStream>
         inline OutputStream& encode(InputStream& is, OutputStream& os) {

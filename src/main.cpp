de7b22d7230cb4d1d5347025a26ca8534e173857
mikuriya <mickly9@hotmail.com>
2018-06-14 11:12:57 +0900

    mod

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
 #include <iostream>
-#include <map>
-#include <chrono>
 
 #if defined(ENABLE_REAL_3D_LED_CUBE)
 #include "spi.h"
@@ -11,84 +9,38 @@
 namespace asio = boost::asio;
 namespace ip = asio::ip;
 
-/** ルックアップテーブルのサイズ */
-const size_t lut_size = 256;
-
-/** LEDパケットのサイズ */
-const size_t packet_size = 8192;
-
-/** パケット型 */
-typedef std::vector<uint8_t> packet_type;
-
-/** 履歴の保存期間 */
-const size_t time_limit_ms = 500;
-
-/** 履歴のバリュー */
-typedef std::pair<std::chrono::system_clock::time_point, packet_type> history_value_type;
-
-/** 履歴のキー */
-typedef ip::address history_key_type;
-
-/** 履歴の型 */
-typedef std::map<history_key_type, history_value_type> history_type;
-
-/*!
- 履歴にパケットを追加する
- @param[in] packet 追加するパケット
- @param[out] history 履歴
- */
-template<size_t N>
-void add_history(ip::address const & address, boost::array<char, N> const & received, history_type & history)
+/** ルックアップテーブル */
+struct Lut
 {
-    packet_type packet(packet_size);
-    for(int i = 0; i < packet.size(); ++i){
-        packet[i] = received[i];
-    }
-    history[address] = std::make_pair(std::chrono::system_clock::now(), packet);
-}
+    uint8_t m[256];
 
-/*!
- 古い履歴を削除する
- @param[out] history 履歴
- */
-void erase_old_history(history_type & history)
-{
-    auto now = std::chrono::system_clock::now();
-    std::vector<ip::address> erase_list;
-    for(auto it = history.begin(); it != history.end(); ++it){
-        auto time = it->second.first;
-        auto d = static_cast<int>(std::chrono::duration_cast<std::chrono::milliseconds>(now - time).count());
-        if(time_limit_ms < d){
-            erase_list.push_back(it->first);
+    Lut() {
+        double gamma = 0.6;
+        for (int ix = 0; ix < sizeof(m); ++ix){
+            m[ix] = round(255 * pow(ix / 255.0, 1.0 / gamma));
         }
     }
-    for(auto it = erase_list.begin(); it != erase_list.end(); ++it){
-        history.erase(*it);
-    }
-}
+};
+
+/** LEDパケットのサイズ */
+const size_t led_packet_size = 8192;
 
 /*!
  3D LED CUBEへデータを転送する
  @param[in] history 履歴
  @param[in] lut ルックアップテーブル
  */
-void send_led(history_type const & history, uint8_t const (&lut)[lut_size])
+template<size_t N>
+void send_led(boost::array<char, N> const & buf, Lut const & lut)
 {
-    packet_type packet(packet_size);
-    for(size_t i = 0; i < packet.size(); ++i){
-        packet[i] = 0;
-        for(auto it = history.cbegin(); it != history.cend(); ++it){
-            packet[i] = std::max(packet[i], it->second.second[i]);
-        }
-    }
     int m[16][32][8] = { 0 };
     for (int x = 0, i = 0; x < 16; ++x){
         for (int y = 0; y < 32; ++y){
             for (int z = 0; z < 8; ++z, ++i){
-                char r = (packet[i * 2] & 0xF8);
-                char g = ((packet[i * 2] & 0x07) << 5) + (((packet[i * 2 + 1] & 0xE0) >> 3));
-                char b = (packet[i * 2 + 1] << 3);
-                m[x][y][z] = (lut[(std::uint8_t)r] << 16) + (lut[(std::uint8_t)g] << 8) + (lut[(std::uint8_t)b] << 0);
+                char r = (buf[i * 2] & 0xF8);
+                char g = ((buf[i * 2] & 0x07) << 5) + (((buf[i * 2 + 1] & 0xE0) >> 3));
+                char b = (buf[i * 2 + 1] << 3);
+                m[x][y][z] = (lut.m[(std::uint8_t)r] << 16) + (lut.m[(std::uint8_t)g] << 8) + (lut.m[(std::uint8_t)b] << 0);
             }
         }
     }
@@ -97,39 +49,21 @@ void send_led(history_type const & history, uint8_t const (&lut)[lut_size])
 #endif
 }
 
-/*!
- ルックアップテーブルを作る
- @param[out] lut
- */
-void make_lut(uint8_t (&lut)[lut_size])
-{
-    double gamma = 0.6;
-    for (int ix = 0; ix < lut_size; ++ix){
-        lut[ix] = round(255 * pow(ix / 255.0, 1.0 / gamma));
-    }
-}
-
 int main(int argc, const char * argv[]) {
-    
-    uint8_t lut[lut_size] = { 0 };
-    make_lut(lut);
-    history_type history;
-    
+    Lut lut;
     asio::io_service io_service;
     ushort const port = 9001;
     ip::udp::socket socket(io_service, ip::udp::endpoint(ip::udp::v4(), port));
+    boost::array<char, led_packet_size * 4> buf;
     for (;;){
-        boost::array<char, packet_size * 4> buf;
         ip::udp::endpoint ep;
         boost::system::error_code er;
         size_t len = socket.receive_from(boost::asio::buffer(buf), ep, 0, er);
-        if (len != packet_size) {
+        if (len != led_packet_size) {
             std::cerr << "size error : " << len << "\n" << std::endl;
             continue;
         }
-        add_history(ep.address(), buf, history);
-        erase_old_history(history);
-        send_led(history, lut);
+        send_led(buf, lut);
     }
     return 0;
 }

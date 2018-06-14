d70396bc9c3769fda57906baf0299314933314bd
fxat00768 <hiroshi.mikuriya@fxat.co.jp>
2018-06-14 13:27:59 +0900

    mod indent

--- main.cpp
+++ main.cpp
@@ -67,7 +67,7 @@ int main(int argc, const char * argv[]) {
         std::cerr << "failed to init bcm2835." << std::endl;
         return 1;
     }
-	led::LookupTable const lut(0.6);
+    led::LookupTable const lut(0.6);
     asio::io_service io_service;
     ushort const port = 9001;
     ip::udp::socket socket(io_service, ip::udp::endpoint(ip::udp::v4(), port));
